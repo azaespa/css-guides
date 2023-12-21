af1ddda90fc8bc792bcdbbf174fe995063a69625
Paul James Azañes <pauljamesazanes@gmail.com>
2023-12-21 18:48:36 +0000

    justify content guide

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const flexWrap = document.querySelector("#flexWrapValue");
 const flexDirection = document.querySelector("#flexDirectionValue");
+const justifyContent = document.querySelector("#justifyContentValue");
+
 const itemsFlexWrap = document.querySelector("#itemsFlexWrap");
 const itemsFlexDirection = document.querySelector("#itemsFlexDirection");
+const itemsJustifyContent = document.querySelector("#itemsJustifyContent");
+
 const typingSim = document.querySelector(".typing-simulator");
 
 flexWrap.addEventListener("keypress", (event) => {
@@ -18,10 +22,21 @@ flexDirection.addEventListener("keypress", (event) => {
     }
 })
 
+justifyContent.addEventListener("keypress", (event) =>  {
+    if (event.key == "Enter") {
+        event.preventDefault()
+        justifyContent.blur();
+    }
+})
+
 flexWrap.addEventListener("blur", () => {
     itemsFlexWrap.style.flexWrap = flexWrap.innerText;
 })
 
 flexDirection.addEventListener("blur", () => {
     itemsFlexDirection.style.flexDirection = flexDirection.innerText;
+})
+
+justifyContent.addEventListener("blur", () => {
+    itemsJustifyContent.style.justifyContent = justifyContent.innerText;
 })
