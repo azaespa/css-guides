5eedeedd2150aad71445d179fde8b8c691c0e354
azaespa <pauljamesazanes@gmail.com>
2023-12-25 23:44:09 +0800

    flex order guide

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const flexWrap = document.querySelector("#flexWrapValue");
 const flexDirection = document.querySelector("#flexDirectionValue");
 const justifyContent = document.querySelector("#justifyContentValue");
 const alignItems = document.querySelector("#alignItemsValue");
+const flexOrderItem1 = document.querySelector("#flexOrderItem1Value");
+const flexOrderItem2 = document.querySelector("#flexOrderItem2Value");
+const flexOrderItem3 = document.querySelector("#flexOrderItem3Value");
+const flexOrderItem4 = document.querySelector("#flexOrderItem4Value");
 
 const itemsFlexWrap = document.querySelector("#itemsFlexWrap");
 const itemsFlexDirection = document.querySelector("#itemsFlexDirection");
 const itemsJustifyContent = document.querySelector("#itemsJustifyContent");
 const itemsAlignItems = document.querySelector("#itemsAlignItems");
+const itemsFlexOrder = document.querySelector("#itemsFlexOrder");
 
 const typingSim = document.querySelector(".typing-simulator");
 
@@ -24,17 +29,45 @@ flexDirection.addEventListener("keypress", (event) => {
     }
 })
 
-justifyContent.addEventListener("keypress", (event) =>  {
+justifyContent.addEventListener("keypress", (event) => {
     if (event.key == "Enter") {
         event.preventDefault()
         justifyContent.blur();
     }
 })
 
-alignItems.addEventListener("keypress", (event) =>  {
+alignItems.addEventListener("keypress", (event) => {
     if (event.key == "Enter") {
         event.preventDefault()
         alignItems.blur();
+    }
+})
+
+flexOrderItem1.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault()
+        flexOrderItem1.blur();
+    }
+})
+
+flexOrderItem2.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault()
+        flexOrderItem2.blur();
+    }
+})
+
+flexOrderItem3.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault()
+        flexOrderItem3.blur();
+    }
+})
+
+flexOrderItem4.addEventListener("keypress", (event) => {
+    if (event.key == "Enter") {
+        event.preventDefault()
+        flexOrderItem4.blur();
     }
 })
 
@@ -52,4 +85,24 @@ justifyContent.addEventListener("blur", () => {
 
 alignItems.addEventListener("blur", () => {
     itemsAlignItems.style.alignItems = alignItems.innerText;
+})
+
+flexOrderItem1.addEventListener("blur", () => {
+    const item1 = itemsFlexOrder.querySelector("#flexOrderItem1");
+    item1.style.order = flexOrderItem1.innerText;
+})
+
+flexOrderItem2.addEventListener("blur", () => {
+    const item1 = itemsFlexOrder.querySelector("#flexOrderItem2");
+    item1.style.order = flexOrderItem2.innerText;
+})
+
+flexOrderItem3.addEventListener("blur", () => {
+    const item1 = itemsFlexOrder.querySelector("#flexOrderItem3");
+    item1.style.order = flexOrderItem3.innerText;
+})
+
+flexOrderItem4.addEventListener("blur", () => {
+    const item1 = itemsFlexOrder.querySelector("#flexOrderItem4");
+    item1.style.order = flexOrderItem4.innerText;
 })
